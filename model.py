0c36e1446089afa605aca5778805c5899773a279
Mithul <mithulmathi@gmail.com>
2019-11-13 20:34:36 -0600

    Modified code to accept speech input for yesno data

--- model.py
+++ model.py
@@ -16,7 +16,6 @@ class Encoder(nn.Module):
 
     def forward(self, src):
         #src = [src sent len, batch size]
-        src = src.view(src.size(0), src.size(1))
         embedded = self.dropout(self.embedding(src))
 
         #embedded = [src sent len, batch size, emb dim]
@@ -39,6 +38,40 @@ class Encoder(nn.Module):
         #hidden = [batch size, dec hid dim]
         return outputs, hidden
 
+class WavEncoder(nn.Module):
+    def __init__(self, input_dim, emb_dim, enc_hid_dim, dec_hid_dim, dropout):
+        super().__init__()
+
+        self.input_dim = input_dim
+        self.embedding = nn.Linear(input_dim, emb_dim)
+        self.rnn = nn.GRU(emb_dim, enc_hid_dim, bidirectional = True)
+        self.fc = nn.Linear(enc_hid_dim * 2, dec_hid_dim)
+        self.dropout = nn.Dropout(dropout)
+
+    def forward(self, src):
+        #src = [src sent len, batch size]
+        # src = src.view(src.size(0), src.size(1))
+        embedded = self.dropout(self.embedding(src))
+
+        #embedded = [src sent len, batch size, emb dim]
+        outputs, hidden = self.rnn(embedded)
+
+        #outputs = [src sent len, batch size, hid dim * num directions]
+        #hidden = [n layers * num directions, batch size, hid dim]
+
+        #hidden is stacked [forward_1, backward_1, forward_2, backward_2, ...]
+        #outputs are always from the last layer
+
+        #hidden [-2, :, : ] is the last of the forwards RNN
+        #hidden [-1, :, : ] is the last of the backwards RNN
+
+        #initial decoder hidden is final hidden state of the forwards and backwards
+        #  encoder RNNs fed through a linear layer
+        hidden = torch.tanh(self.fc(torch.cat((hidden[-2,:,:], hidden[-1,:,:]), dim = 1)))
+
+        #outputs = [src sent len, batch size, enc hid dim * 2]
+        #hidden = [batch size, dec hid dim]
+        return outputs, hidden
 
 class Attention(nn.Module):
     def __init__(self, enc_hid_dim, dec_hid_dim):
